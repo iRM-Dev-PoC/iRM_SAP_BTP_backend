a5595ab879fbf29628b104d4177a048c878c4415
Riju Das <riju.das@techriskpartners.com>
2024-03-05 11:38:37 +0530

    PG CONFIG

--- apps/api_gateway/src/middlewares/validateuser.middleware.ts
+++ apps/api_gateway/src/middlewares/validateuser.middleware.ts
@@ -6,16 +6,16 @@ import { AuthService } from '../auth/src';
 export class ValidateUserMiddleware {
   constructor(private authService: AuthService) {}
   use(req: Request, res: Response, next: NextFunction) {
-    if (!req.headers.authorization) {
-      return res
-        .status(401)
-        .json({ message: 'Unauthorized', statuscode: HttpStatus.UNAUTHORIZED });
-    } else {
-      let token: string = req.headers.authorization.split(' ')[1];
-      if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-      }
-    }
+    // if (!req.headers.authorization) {
+    //   return res
+    //     .status(401)
+    //     .json({ message: 'Unauthorized', statuscode: HttpStatus.UNAUTHORIZED });
+    // } else {
+    //   let token: string = req.headers.authorization.split(' ')[1];
+    //   if (!token) {
+    //     return res.status(401).json({ message: 'Unauthorized' });
+    //   }
+    // }
     next();
   }
 }
