e97fb7740a57c55e2b1f8ff3e527003c2e575ca4
Suman Ghorai <suman.ghorai@techriskpartners.com>
2024-04-08 14:46:28 +0530

    chore:
    add cors configuration, switched to ipv4 from ipv6

--- apps/api_gateway/src/main.ts
+++ apps/api_gateway/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+
+  const corsOptions: cors.CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization', 
+  };
+  app.enableCors(corsOptions);
+
+  // await app.listen(8080);
+  await app.listen(8080, '0.0.0.0');
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
