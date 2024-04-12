da868a566fabfaebc4680e07f75eb28d653d99c2
Suman Ghorai <suman.ghorai@techriskpartners.com>
2024-04-12 22:31:50 +0530

    update sub module
    modify all the API for the sub module and corresponding changes

--- apps/api_gateway/src/submodule_master/dto/submoduleMaster.dto.ts
+++ apps/api_gateway/src/submodule_master/dto/submoduleMaster.dto.ts
@@ -4,19 +4,20 @@
 import { IsNotEmpty, IsNumber } from "class-validator";
 
 export class CreateSubModuleMasterDto {
+    id: number;
     submodule_id: string; //UUID;
   
     @IsNotEmpty()
     submodule_name: string;
     submodule_desc: string;
     @IsNumber()
-    @IsNotEmpty()
+    // @IsNotEmpty()
     parent_module_id_id: number;
   
     @IsNotEmpty()
     display_submodule_name: string;
   
-    @IsNotEmpty()
+    // @IsNotEmpty()
     customer_id_id: string;
     created_on: Date;
     created_by: string;
@@ -24,22 +25,22 @@ export class CreateSubModuleMasterDto {
   
   export class UpdateSubModuleMasterDto {
     @IsNotEmpty()
-    id: string;
-  
+    id: number;
+    submodule_name: string;
     submodule_id: string;
     submodule_desc: string;
     @IsNumber()
     parent_module_id_id: number;
     display_submodule_name: string;
-  
-    @IsNotEmpty()
+
+    // @IsNotEmpty()
     customer_id_id: string;
     changed_on: Date;
     changed_by: string;
   }
   
   export class ReadSubModuleMasterDto {
-    id: string;
+    id: number;
     module_id: string;
     display_module_name: string;
     module_desc: string;
@@ -54,8 +55,8 @@ export class CreateSubModuleMasterDto {
   
   export class DeleteSubModuleMasterDto {
     @IsNotEmpty()
-    id: string;
-    @IsNotEmpty()
+    id: number;
+    // @IsNotEmpty()
     customer_id: string;
   }
   //#endregion moduleMaster
