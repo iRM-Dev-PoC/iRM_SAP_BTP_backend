37904b73a2405d13ccad71930182ab3cab3d8b59
Suman Ghorai <suman.ghorai@techriskpartners.com>
2024-04-17 14:38:59 +0530

    feat: update submodule master
    modify all the API for submodule master

--- apps/api_gateway/src/submodule_master/dto/submoduleMaster.dto.ts
+++ apps/api_gateway/src/submodule_master/dto/submoduleMaster.dto.ts
@@ -1,63 +1,55 @@
-
 //#region moduleMaster
 
-import { IsNotEmpty, IsNumber } from "class-validator";
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateSubModuleMasterDto {
-    id: number;
-    submodule_id: string; //UUID;
-  
-    @IsNotEmpty()
-    submodule_name: string;
-    submodule_desc: string;
-    @IsNumber()
-    // @IsNotEmpty()
-    parent_module_id_id: number;
-  
-    @IsNotEmpty()
-    display_submodule_name: string;
-  
-    // @IsNotEmpty()
-    customer_id_id: string;
-    created_on: Date;
-    created_by: string;
-  }
-  
-  export class UpdateSubModuleMasterDto {
-    @IsNotEmpty()
-    id: number;
-    submodule_name: string;
-    submodule_id: string;
-    submodule_desc: string;
-    @IsNumber()
-    parent_module_id_id: number;
-    display_submodule_name: string;
+  id: number;
+  @IsNotEmpty()
+  submodule_name: string;
+  submodule_desc: string;
+  @IsNumber()
+  @IsNotEmpty()
+  parent_module_id: number;
+  @IsNotEmpty()
+  display_submodule_name: string;
+  @IsNotEmpty()
+  customer_id: number;
+  created_on: Date;
+  created_by: number;
+}
 
-    // @IsNotEmpty()
-    customer_id_id: string;
-    changed_on: Date;
-    changed_by: string;
-  }
-  
-  export class ReadSubModuleMasterDto {
-    id: number;
-    module_id: string;
-    display_module_name: string;
-    module_desc: string;
-    parent_module_id_id: string;
-    is_active: string;
-    created_on: Date;
-    created_by: string;
-    changed_on: Date;
-    changed_by: string;
-    customer_id_id: string;
-  }
-  
-  export class DeleteSubModuleMasterDto {
-    @IsNotEmpty()
-    id: number;
-    // @IsNotEmpty()
-    customer_id: string;
-  }
-  //#endregion moduleMaster
-  
+export class UpdateSubModuleMasterDto {
+  @IsNotEmpty()
+  id: number;
+  submodule_name: string;
+  submodule_id: string;
+  submodule_desc: string;
+  @IsNumber()
+  parent_module_id: number;
+  display_submodule_name: string;
+  @IsNotEmpty()
+  customer_id: number;
+  changed_on: Date;
+  changed_by: number;
+}
+
+export class ReadSubModuleMasterDto {
+  id: number;
+  display_module_name: string;
+  module_desc: string;
+  parent_module_id: number;
+  is_active: string;
+  created_on: Date;
+  created_by: number;
+  changed_on: Date;
+  changed_by: number;
+  customer_id: number;
+}
+
+export class DeleteSubModuleMasterDto {
+  @IsNotEmpty()
+  id: number;
+  @IsNotEmpty()
+  customer_id: number;
+}
+//#endregion moduleMaster
--- apps/api_gateway/src/submodule_master/submodule_master.service.ts
+++ apps/api_gateway/src/submodule_master/submodule_master.service.ts
@@ -17,93 +17,30 @@ export class SubmoduleMasterService {
     private readonly appService: AppService,
   ) {}
 
-  // async CreateSubModule(
-  //   currentUser: CurrentUserDto,
-  //   createSubModule: CreateSubModuleMasterDto,
-  // ): Promise<ResponseDto> {
-  //   let tx = cds.transaction();
-
-  //   try {
-  //     createSubModule.submodule_id = uuidv4();
-  //     createSubModule.created_on = new Date();
-  //     createSubModule.created_by = currentUser.user_id;
-  //     createSubModule.submodule_name =
-  //       createSubModule.submodule_name.toUpperCase();
-  //     createSubModule.display_submodule_name =
-  //       createSubModule.display_submodule_name.toUpperCase();
-  //     if (
-  //       createSubModule.submodule_name ||
-  //       createSubModule.display_submodule_name
-  //     ) {
-  //       let exisingSubModule = await tx.run(
-  //         SELECT.from('PCF_DB_SUBMODULE_MASTER')
-  //           .columns('submodule_name', 'display_submodule_name', 'is_active')
-  //           .where(
-  //             `submodule_name = '${createSubModule.submodule_name}' OR display_submodule_name = '${createSubModule.display_submodule_name}' and is_active = 'Y'`,
-  //           ),
-  //       );
-  //       // console.log('exisingModule', exisingModule);
-  //       if (exisingSubModule && exisingSubModule.length > 0) {
-  //         await tx.commit();
-  //         return {
-  //           statuscode: HttpStatus.CONFLICT,
-  //           message: 'SubModule already exists',
-  //           data: exisingSubModule,
-  //         };
-  //       }
-  //     }
-
-  //     let result = await tx.run(
-  //       INSERT.into('PCF_DB_SUBMODULE_MASTER').entries(createSubModule),
-  //     );
-  //     if (!result || result == 0) {
-  //       await tx.commit();
-  //       return {
-  //         statuscode: HttpStatus.INTERNAL_SERVER_ERROR,
-  //         message: 'Submodule creation failed',
-  //         data: result,
-  //       };
-  //     }
-  //     await tx.commit();
-  //     return {
-  //       statuscode: HttpStatus.CREATED,
-  //       message: 'Submodule created successfully',
-  //       data: createSubModule,
-  //     };
-  //   } catch (error) {
-  //     await tx.rollback();
-  //     return {
-  //       statuscode: 500,
-  //       message: 'Error while creating submodule',
-  //       data: error,
-  //     };
-  //   }
-  // }
-
   async CreateSubModule(
     // currentUser: CurrentUserDto,
     createSubModule: CreateSubModuleMasterDto,
   ): Promise<ResponseDto> {
-    const hanaOptions = this.databaseService.getHanaOptions();
     try {
-      const tableName = 'PCF_DB_SUBMODULE_MASTER';
-      const nextUserId = await this.appService.getLastUserId(tableName);
+      const db = await cds.connect.to('db');
 
-      createSubModule.id = nextUserId;
-      createSubModule.submodule_id = uuidv4();
-      createSubModule.created_on = new Date();
-      createSubModule.created_by = '1';
-      createSubModule.submodule_name.toUpperCase();
-      createSubModule.display_submodule_name.toUpperCase();
+      createSubModule.created_by = 1;
+      createSubModule.submodule_name =
+        createSubModule.submodule_name.toUpperCase();
+      createSubModule.display_submodule_name =
+        createSubModule.display_submodule_name.toUpperCase();
 
       if (
         createSubModule.submodule_name ||
         createSubModule.display_submodule_name
       ) {
-        let existingSubModule = await this.databaseService.executeQuery(
-          `SELECT submodule_name, display_submodule_name, is_active FROM ${hanaOptions.schema}.PCF_DB_SUBMODULE_MASTER WHERE submodule_name = '${createSubModule.submodule_name}' OR display_submodule_name = '${createSubModule.display_submodule_name}' AND is_active = 'Y'`,
-          hanaOptions,
+        const whereClause = cds.parse.expr(
+          `SUBMODULE_NAME = '${createSubModule.submodule_name}' OR DISPLAY_SUBMODULE_NAME = '${createSubModule.display_submodule_name}' AND IS_ACTIVE = 'Y'`,
         );
+
+        const existingSubModule = await db
+          .read('PCF_DB_SUBMODULE_MASTER')
+          .where(whereClause);
 
         if (existingSubModule && existingSubModule.length > 0) {
           return {
@@ -114,16 +51,22 @@ export class SubmoduleMasterService {
         }
       }
 
-      let result = await this.databaseService.executeQuery(
-        `INSERT INTO ${hanaOptions.schema}.PCF_DB_SUBMODULE_MASTER (id, submodule_id, submodule_name, display_submodule_name, submodule_desc, created_on, created_by, is_active) VALUES ('${createSubModule.id}', '${createSubModule.submodule_id}', '${createSubModule.submodule_name}', '${createSubModule.display_submodule_name}', '${createSubModule.submodule_desc}', TO_TIMESTAMP('${createSubModule.created_on.toISOString().slice(0, 23)}', 'YYYY-MM-DD"T"HH24:MI:SS.FF9'), '${createSubModule.created_by}', 'Y')`,
-        hanaOptions,
-      );
+      const createdSubModule = await INSERT.into(
+        'PCF_DB_SUBMODULE_MASTER',
+      ).entries({
+        SUBMODULE_NAME: `${createSubModule.submodule_name}`,
+        DISPLAY_SUBMODULE_NAME: `${createSubModule.display_submodule_name}`,
+        SUBMODULE_DESC: `${createSubModule.submodule_desc}`,
+        CREATED_BY: createSubModule.created_by,
+        CUSTOMER_ID: `${createSubModule.customer_id}`,
+        PARENT_MODULE_ID: `${createSubModule.parent_module_id}`,
+      });
 
-      if (!result || result === 0) {
+      if (!createdSubModule || createdSubModule === 0) {
         return {
           statuscode: HttpStatus.INTERNAL_SERVER_ERROR,
           message: 'Submodule creation failed',
-          data: result,
+          data: createdSubModule,
         };
       }
 
@@ -141,103 +84,49 @@ export class SubmoduleMasterService {
     }
   }
 
-  // async UpdateSubModule(
-  //   currentUser: CurrentUserDto,
-  //   updateSubModule: UpdateSubModuleMasterDto,
-  // ): Promise<ResponseDto> {
-  //   let tx = cds.transaction();
-  //   try {
-  //     updateSubModule.changed_on = new Date();
-  //     updateSubModule.changed_by = currentUser.user_id;
-
-  //     if (updateSubModule.display_submodule_name) {
-  //       let exisingSubModule = await tx.run(
-  //         SELECT.from('PCF_DB_SUBMODULE_MASTER')
-  //           .columns('submodule_name', 'display_submodule_name', 'is_active')
-  //           .where(
-  //             `id !='${updateSubModule.id}' and display_submodule_name = '${updateSubModule.display_submodule_name.toUpperCase()}' and is_active = 'Y'`,
-  //           ),
-  //       );
-  //       // console.log('exisingSubModule', exisingSubModule);
-  //       if (exisingSubModule && exisingSubModule.length > 0) {
-  //         await tx.commit();
-  //         return {
-  //           statuscode: HttpStatus.CONFLICT,
-  //           message: 'SubModule already exists',
-  //           data: exisingSubModule,
-  //         };
-  //       }
-  //     }
-
-  //     let result = await tx.run(
-  //       UPDATE('PCF_DB_SUBMODULE_MASTER')
-  //         .set({
-  //           submodule_id: updateSubModule.submodule_id,
-  //           submodule_desc: updateSubModule.submodule_desc,
-  //           parent_module_id_id: updateSubModule.parent_module_id_id,
-  //           display_submodule_name: updateSubModule.display_submodule_name,
-  //           changed_on: updateSubModule.changed_on,
-  //           changed_by: updateSubModule.changed_by,
-  //         })
-  //         .where(
-  //           `id = '${updateSubModule.id}' and is_active = 'Y' and customer_id_id = '${updateSubModule.customer_id_id}'`,
-  //         ),
-  //     );
-  //     await tx.commit();
-  //     return {
-  //       statuscode: HttpStatus.OK,
-  //       message: 'Submodule updated successfully',
-  //       data: updateSubModule,
-  //     };
-  //   } catch (error) {
-  //     await tx.rollback();
-  //     return {
-  //       statuscode: 500,
-  //       message: 'Error while updating submodule',
-  //       data: error,
-  //     };
-  //   }
-  // }
-
   async UpdateSubModule(
     // currentUser: CurrentUserDto,
     updateSubModule: UpdateSubModuleMasterDto,
   ): Promise<ResponseDto> {
-    const hanaOptions = this.databaseService.getHanaOptions();
     try {
+      const db = await cds.connect.to('db');
+
       updateSubModule.changed_on = new Date();
-      updateSubModule.changed_by = '2';
+      updateSubModule.changed_by = 2;
 
       if (updateSubModule.display_submodule_name) {
-        let exisingSubModule = await this.databaseService.executeQuery(
-          `SELECT submodule_name, display_submodule_name, is_active FROM ${hanaOptions.schema}.PCF_DB_SUBMODULE_MASTER WHERE id != '${updateSubModule.id}' AND display_submodule_name = '${updateSubModule.display_submodule_name.toUpperCase()}' AND is_active = 'Y'`,
-          hanaOptions,
+        const whereClause = cds.parse.expr(
+          `ID != '${updateSubModule.id}' AND CUSTOMER_ID ='${updateSubModule.customer_id}' AND DISPLAY_SUBMODULE_NAME = '${updateSubModule.display_submodule_name.toUpperCase()}' AND IS_ACTIVE = 'Y'`,
         );
 
-        if (exisingSubModule && exisingSubModule.length > 0) {
+        const existingSubModule = await db
+          .read('PCF_DB_SUBMODULE_MASTER')
+          .where(whereClause);
+
+        if (existingSubModule && existingSubModule.length > 0) {
           return {
             statuscode: HttpStatus.CONFLICT,
             message: 'SubModule already exists',
-            data: exisingSubModule,
+            data: existingSubModule,
           };
         }
       }
 
-      let result = await this.databaseService.executeQuery(
-        `UPDATE ${hanaOptions.schema}.PCF_DB_SUBMODULE_MASTER
-         SET
-            submodule_name = '${updateSubModule.submodule_name}',
-            submodule_desc = '${updateSubModule.submodule_desc}',
-            display_submodule_name = '${updateSubModule.display_submodule_name}',
-            changed_on = TO_TIMESTAMP('${updateSubModule.changed_on.toISOString().slice(0, 23)}', 'YYYY-MM-DD"T"HH24:MI:SS.FF9'),
-            changed_by = '${updateSubModule.changed_by}'
-         WHERE
-            id = '${updateSubModule.id}' AND
-            is_active = 'Y' `,
-        hanaOptions,
-      );
+      const updatedSubModule = await UPDATE('PCF_DB_SUBMODULE_MASTER')
+        .set({
+          SUBMODULE_NAME: updateSubModule.submodule_name,
+          SUBMODULE_DESC: updateSubModule.submodule_desc,
+          DISPLAY_SUBMODULE_NAME: updateSubModule.display_submodule_name,
+          CHANGED_ON: updateSubModule.changed_on.toISOString(),
+          CHANGED_BY: updateSubModule.changed_by,
+        })
+        .where({
+          ID: updateSubModule.id,
+          CUSTOMER_ID: updateSubModule.customer_id,
+          IS_ACTIVE: 'Y',
+        });
 
-      await this.databaseService.executeQuery(`COMMIT`, hanaOptions);
+      await db.run(`COMMIT`);
 
       return {
         statuscode: HttpStatus.OK,
@@ -253,73 +142,15 @@ export class SubmoduleMasterService {
     }
   }
 
-  // async GetSubModule(id, customer_id): Promise<ResponseDto> {
-  //   let tx = cds.transaction();
-  //   try {
-  //     let result = await tx.run(
-  //       SELECT.from('PCF_DB_SUBMODULE_MASTER')
-  //         .columns(
-  //           'id',
-  //           'submodule_id',
-  //           'submodule_desc',
-  //           'parent_module_id_id',
-  //           'display_submodule_name',
-  //           'customer_id_id',
-  //           'created_on',
-  //           'created_by',
-  //           'changed_on',
-  //           'changed_by',
-  //         )
-  //         .where(
-  //           `id='${Number(id)}' and customer_id_id='${Number(customer_id)}' and is_active = 'Y'`,
-  //         ),
-  //     );
-  //     if (!result || result.length == 0) {
-  //       await tx.commit();
-  //       return {
-  //         statuscode: HttpStatus.NOT_FOUND,
-  //         message: 'Submodule not found',
-  //         data: result,
-  //       };
-  //     }
-  //     await tx.commit();
-  //     return {
-  //       statuscode: HttpStatus.OK,
-  //       message: 'Submodule fetched successfully',
-  //       data: result,
-  //     };
-  //   } catch (error) {
-  //     await tx.rollback();
-  //     return {
-  //       statuscode: 500,
-  //       message: 'Error while fetching submodule',
-  //       data: error,
-  //     };
-  //   }
-  // }
-
-  async GetSubModule(id: number, customer_id: number): Promise<ResponseDto> {
-    const hanaOptions = this.databaseService.getHanaOptions();
+  async GetSubModule(id, customer_id): Promise<ResponseDto> {
     try {
-      const result = await this.databaseService.executeQuery(
-        `SELECT 
-                id, 
-                submodule_id, 
-                submodule_desc, 
-                parent_module_id_id, 
-                display_submodule_name, 
-                customer_id_id, 
-                created_on, 
-                created_by, 
-                changed_on, 
-                changed_by 
-             FROM 
-                ${hanaOptions.schema}.PCF_DB_SUBMODULE_MASTER 
-             WHERE 
-                id = '${Number(id)}' AND 
-                is_active = 'Y'`,
-        hanaOptions,
+      const db = await cds.connect.to('db');
+
+      const whereClause = cds.parse.expr(
+        `ID = '${Number(id)}' AND IS_ACTIVE = 'Y'`,
       );
+
+      const result = await db.read('PCF_DB_SUBMODULE_MASTER').where(whereClause);
 
       if (!result || result.length === 0) {
         return {
@@ -343,109 +174,72 @@ export class SubmoduleMasterService {
     }
   }
 
-  // async DeleteSubModule(
-  //   currentUser: CurrentUserDto,
-  //   deleteSubModule: DeleteSubModuleMasterDto,
-  // ): Promise<ResponseDto> {
-  //   let tx = cds.transaction();
-  //   try {
-  //     let result = await tx.run(
-  //       UPDATE('PCF_DB_SUBMODULE_MASTER')
-  //         .set({
-  //           is_active: 'N',
-  //           changed_on: new Date(),
-  //           changed_by: currentUser.user_id,
-  //         })
-  //         .where(
-  //           `id = '${deleteSubModule.id}' and customer_id_id = '${deleteSubModule.customer_id}' and is_active = 'Y'`,
-  //         ),
-  //     );
-  //     await tx.commit();
-  //     if (!result || result == 0) {
-  //       return {
-  //         statuscode: HttpStatus.NOT_FOUND,
-  //         message: 'Submodule not found for delete',
-  //         data: result,
-  //       };
-  //     }
-  //     return {
-  //       statuscode: HttpStatus.OK,
-  //       message: 'Submodule deleted successfully',
-  //       data: result,
-  //     };
-  //   } catch (error) {
-  //     await tx.rollback();
-  //     return {
-  //       statuscode: 500,
-  //       message: 'Error while deleting submodule',
-  //       data: error,
-  //     };
-  //   }
-  // }
-
   async DeleteSubModule(
     // currentUser: CurrentUserDto,
     deleteSubModule: DeleteSubModuleMasterDto,
   ): Promise<ResponseDto> {
-    const hanaOptions = this.databaseService.getHanaOptions();
     try {
-      let query = `
-        DELETE FROM ${hanaOptions.schema}.PCF_DB_SUBMODULE_MASTER
-        WHERE
-          id = '${deleteSubModule.id}' AND
-          is_active = 'Y'
-      `;
+      const db = await cds.connect.to('db');
 
-      let module = await this.databaseService.executeQuery(query, hanaOptions);
+      const affectedRows = await UPDATE('PCF_DB_SUBMODULE_MASTER')
+        .set({ IS_ACTIVE: 'N' })
+        .where({
+          ID: deleteSubModule.id,
+          CUSTOMER_ID: deleteSubModule.customer_id,
+          IS_ACTIVE: 'Y',
+        });
 
-      if (module === 0) {
+      if (affectedRows === 0) {
         return {
           statuscode: HttpStatus.NOT_FOUND,
-          message: 'Module not found for deletion',
-          data: module,
+          message: 'Submodule not found for deletion',
+          data: null,
         };
       }
 
       return {
         statuscode: HttpStatus.OK,
-        message: 'Module deleted successfully',
-        data: module,
+        message: 'Submodule deleted successfully',
+        data: affectedRows,
       };
     } catch (error) {
       return {
-        statuscode: error.status,
-        message: error.message,
-        data: null,
+        statuscode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Error while deleting submodule',
+        data: error,
       };
     }
   }
 
   async GetAllSubModules() // currentUser: CurrentUserDto,
   : Promise<ResponseDto> {
-    const hanaOptions = this.databaseService.getHanaOptions();
     try {
-      let query = `SELECT * FROM ${hanaOptions.schema}.PCF_DB_SUBMODULE_MASTER`;
+      const db = await cds.connect.to('db');
 
-      let modules = await this.databaseService.executeQuery(query, hanaOptions);
+      const whereClause = cds.parse.expr(`IS_ACTIVE = 'Y'`);
 
-      if (!modules || modules.length === 0) {
+      const submodules = await db
+        .read('PCF_DB_SUBMODULE_MASTER')
+        .where(whereClause);
+
+      if (!submodules || submodules.length === 0) {
         return {
           statuscode: HttpStatus.NOT_FOUND,
-          message: 'No modules found',
-          data: modules,
+          message: 'No submodules found',
+          data: submodules,
         };
       }
 
       return {
         statuscode: HttpStatus.OK,
-        message: 'Modules fetched successfully',
-        data: modules,
+        message: 'Submodules fetched successfully',
+        data: submodules,
       };
     } catch (error) {
       return {
-        statuscode: error.status,
-        message: error.message,
-        data: null,
+        statuscode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Failed to fetch submodules',
+        data: error,
       };
     }
   }
